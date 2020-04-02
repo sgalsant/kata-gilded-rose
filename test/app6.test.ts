b64dc1200e7191e2529834e8d02f13c34835e698
santiago galván <sgalsant@gmail.com>
2020-04-02 23:11:45 +0100

    comment

--- test/app6.test.ts
+++ test/app6.test.ts
@@ -13,8 +13,8 @@ describe ('testing kata Gilded Rose', function() {
         return new Item("aged bried", sellIn, quality,
             new Intervalos([
                 1, 0, Number.POSITIVE_INFINITY,   // incrementa 1 desde 0 hasta infinito
-                2, Number.NEGATIVE_INFINITY, -1
-            ])); // incrementa 2 desde -infinito hasta -1
+                2, Number.NEGATIVE_INFINITY, -1    // incrementa 2 desde -infinito hasta -1
+            ]));
     }
 
     function createSulfurasItem(sellIn: number, quality: number) {
@@ -29,16 +29,16 @@ describe ('testing kata Gilded Rose', function() {
                 1, 11, Number.POSITIVE_INFINITY,  // incrementa 1 desde 11 hasta infinito
                 2, 5, 10,                         // incrementa 2 desde 5 hasta 10
                 3, 0, 4,                          // incrementa 3 desde 0 hasta 4
-                Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, -1
-            ]));  // resta todo (infinito) desde -infinito hasta -1
+                Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, -1  // resta todo (infinito) desde -infinito hasta -1
+            ])); 
     }
 
     function createConjured(sellIn: number, quality: number) {
         return new Item('conjured', sellIn, quality,
             new Intervalos([
                 -2, 0, Number.POSITIVE_INFINITY, // decrementa 2 desde 0 hata infinito
-                -4, Number.NEGATIVE_INFINITY, -1
-            ]));  // decrementa 4 desde -infinito hasta -1
+                -4, Number.NEGATIVE_INFINITY, -1 // decrementa 4 desde -infinito hasta -1
+            ]));  
     }
 
     it('Items degradan en 1 la calidad y la fecha de venta', function(){
